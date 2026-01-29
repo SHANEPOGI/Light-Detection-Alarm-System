f8d6ac12d998c97371be9bb72ac51f891f0605ad
Engr.Niño Shane H. Layug <158708733+SHANEPOGI@users.noreply.github.com>
2026-01-29 15:12:50 +0800

    Update lightController.ino

--- LightDetectionAlarmSystem/controller/lightController.ino
+++ LightDetectionAlarmSystem/controller/lightController.ino
@@ -1,1 +1,33 @@
+#ifndef LIGHT_CONTROLLER_H
+#define LIGHT_CONTROLLER_H
+
+#include "ldrModel.ino"
+#include "alarmView.ino"
+#include "pinConfig.ino"
+
+class LightController {
+  private:
+    LDRModel model;
+    AlarmView view;
+
+  public:
+    void begin() {
+      model.begin();
+      view.begin();
+    }
+
+    void update() {
+      int ldrValue = model.readLight();
+
+      if (ldrValue >= LIGHT_THRESHOLD) {
+        view.showBright(ldrValue);
+      } else {
+        view.showDark(ldrValue);
+      }
+
+      delay(500);
+    }
+};
+
+#endif
 
