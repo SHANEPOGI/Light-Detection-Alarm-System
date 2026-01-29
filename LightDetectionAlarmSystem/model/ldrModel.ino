e71b1a5c8898b425dcc2ece75a219d8f08837e3f
Engr.Niño Shane H. Layug <158708733+SHANEPOGI@users.noreply.github.com>
2026-01-29 15:11:58 +0800

    Update ldrModel.ino

--- LightDetectionAlarmSystem/model/ldrModel.ino
+++ LightDetectionAlarmSystem/model/ldrModel.ino
@@ -1,1 +1,18 @@
+#ifndef LDR_MODEL_H
+#define LDR_MODEL_H
+
+#include "pinConfig.ino"
+
+class LDRModel {
+  public:
+    void begin() {
+      pinMode(LDR_PIN, INPUT);
+    }
+
+    int readLight() {
+      return analogRead(LDR_PIN);
+    }
+};
+
+#endif
 
