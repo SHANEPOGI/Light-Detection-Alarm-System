c4ceb510fcc7caa4925de8c75e0caf3d30322fba
Engr.Niño Shane H. Layug <158708733+SHANEPOGI@users.noreply.github.com>
2026-01-29 15:11:35 +0800

    Update pinConfig.ino

--- LightDetectionAlarmSystem/config/pinConfig.ino
+++ LightDetectionAlarmSystem/config/pinConfig.ino
@@ -1,1 +1,14 @@
+#ifndef PIN_CONFIG_H
+#define PIN_CONFIG_H
+
+// Pin Definitions
+const int LDR_PIN = A0;
+const int GREEN_LED = 4;
+const int ORANGE_LED = 3;
+const int BUZZER_PIN = 2;
+
+// Threshold
+const int LIGHT_THRESHOLD = 60;
+
+#endif
 
